8089009edbc08a06d51591755092ee70b16eaba5
Joni Lee <jnz.lee@gmail.com>
2022-07-16 11:32:03 +1200

    Inital commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,25 @@
-import '../styles/globals.css'
+import React from 'react'
 import type { AppProps } from 'next/app'
+import '../styles/globals.css'
+import { QueryClient, QueryClientProvider } from 'react-query'
+import { ReactQueryDevtools } from 'react-query/devtools'
+import { createTheme, ThemeProvider, } from '@mui/material/styles';
+import { CssBaseline } from '@mui/material'
+
+
+const theme = createTheme();
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const [queryClient] = React.useState(() => new QueryClient())
+  return (
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Component {...pageProps} />
+      </ThemeProvider>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  )
 }
 
 export default MyApp
